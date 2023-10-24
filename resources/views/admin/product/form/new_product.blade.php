679bc74923ba54dbe320667f3640895b9a33c3f1
unknown <lemanhlinh209@gmail.com>
2023-10-24 17:12:01 +0700

    fff

--- resources/views/admin/product/form/new_product.blade.php
+++ resources/views/admin/product/form/new_product.blade.php
@@ -39,7 +39,7 @@
                                 <!-- text input -->
                                 <div class="form-group">
                                     <label>@lang('form.product.sku')</label>
-                                    <input type="text" class="form-control" name="sku" value="{{ isset($product) ? $product->sku : old('sku') }}">
+                                    <input type="text" class="form-control" name="sku" value="{{ isset($product) ? $product->sku : old('sku') }}" required>
                                     @if ($errors->has('sku'))
                                         <span class="help-block text-danger">
                                             <strong>{{ $errors->first('sku') }}</strong>
