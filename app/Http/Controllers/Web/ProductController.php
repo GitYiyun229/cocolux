252a0708d0d6d1171090049357c24f516fcce610
unknown <lemanhlinh209@gmail.com>
2023-10-25 15:06:47 +0700

    a

--- app/Http/Controllers/Web/ProductController.php
+++ app/Http/Controllers/Web/ProductController.php
@@ -382,7 +382,7 @@ class ProductController extends Controller
         $list_image = json_decode($product->images);
         $stocks = json_decode($product->stocks);
         $product_root = Product::where(['id' => $product->parent_id])->select('id','slug','title','image','brand','description','attributes')->with(['category'])->first();
-        $attribute_value = json_decode($product_root->attributes);
+        $attribute_value = !empty($product_root->attributes)?json_decode($product_root->attributes):'';
         $list_product_parent = ProductOptions::where(['parent_id' => $product->parent_id])->get();
         $products = Product::select('id','title','slug','image','price','category_id','sku')
             ->where(['active' => 1,'category_id' => $product->product->category_id])
