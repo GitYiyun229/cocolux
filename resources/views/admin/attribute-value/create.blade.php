22ad90397c43a9bc33cc40553423e7dcfe29bfd7
unknown <lemanhlinh209@gmail.com>
2023-11-04 16:13:51 +0700

    fix attribute

--- resources/views/admin/attribute-value/create.blade.php
+++ resources/views/admin/attribute-value/create.blade.php
@@ -4,9 +4,9 @@
 
 @section('content')
     <div class="card card-primary card-body">
-        <form action="{{ route('admin.article.store') }}" method="POST" enctype="multipart/form-data">
+        <form action="{{ route('admin.attribute-value.store') }}" method="POST" enctype="multipart/form-data">
             @csrf
-            @include('admin.article.form.inputs')
+            @include('admin.attribute-value.form.inputs')
             <button type="submit" class="btn btn-primary">@lang('form.button.submit')</button>
         </form>
 {{--        <button id="ckfinder-popup" class="button-a button-a-background" style="float: left">Open Popup</button>--}}
