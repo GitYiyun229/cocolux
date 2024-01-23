c659c2c0f7ed4d0f556e6aecffb8e0b65595f4d2
unknown <lemanhlinh209@gmail.com>
2024-01-23 10:54:01 +0700

    ffff

--- resources/views/admin/voucher/index.blade.php
+++ resources/views/admin/voucher/index.blade.php
@@ -3,7 +3,7 @@
 @section('title_file', trans('form.banner.'))
 
 @section('content')
-{{--    <a href="{{ route('admin.banner.create') }}" class="btn btn-primary mb-3"><i class="fa fa-plus"></i> @lang('form.button.create')</a>--}}
+    <a href="{{ route('listCoupons') }}" class="btn btn-primary mb-3" target="_blank">Lấy danh sách coupon</a>
     {!! $dataTable->table(['id' => 'voucher-table', 'class' => 'table table-striped table-bordered table-width-auto']) !!}
 @endsection
 
