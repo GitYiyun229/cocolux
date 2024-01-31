ebfae308014a22f457e3b7acb5778a1e0b92fe67
unknown <lemanhlinh209@gmail.com>
2024-01-31 10:54:59 +0700

    ddd

--- webpack.mix.js
+++ webpack.mix.js
@@ -49,5 +49,4 @@ mix.sass('resources/sass/home.scss', 'public/css/web')
     .sass('resources/sass/hot-deal.scss', 'public/css/web')
     .sass('resources/sass/deal-detail.scss', 'public/css/web')
     .sass('resources/sass/login-admin.scss', 'public/css/web')
-    .purgeCss()
     .sourceMaps().version();
