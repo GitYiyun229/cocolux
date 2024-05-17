db5009ae20298f22076764d44ffbec409b027e17
hoangthong2950 <hoangthong2950@gmail.com>
2024-05-17 15:39:49 +0700

    check 404

--- resources/views/web/404.blade.php
+++ resources/views/web/404.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container py-3 position-relative view-404">
         <div class="view-1">
-            <img src="{{ asset('images/404_cocolux.svg') }}" alt=""
+            <img src="{{ asset('images/404_cocolux1.svg') }}" alt=""
                 class="img-fluid w-100">
         </div>
         <div class="position-absolute w-100 d-flex justify-content-center view-2" >
@@ -12,7 +12,7 @@
                     <path d="M7.825 13L13.425 18.6L12 20L4 12L12 4L13.425 5.4L7.825 11H20V13H7.825Z" fill="black" />
                 </svg>
                 Quay lại trang chủ</a>
-                
+
         </div>
     </div>
 @endsection
