d8b69978effb6f41d50baaf1348a9535439feb3a
hoangthong2950 <hoangthong2950@gmail.com>
2024-05-17 15:03:40 +0700

    check api nhanh log

--- resources/views/web/404.blade.php
+++ resources/views/web/404.blade.php
@@ -1,8 +1,18 @@
 @extends('web.layouts.web')
 
 @section('content')
-    <div class="container py-3">
-        <img src="{{ asset('images/404_cocolux.svg') }}" alt="" width="29" height="29" class="img-fluid w-100">
+    <div class="container py-3 position-relative view-404">
+        <div class="view-1">
+            <img src="{{ asset('images/404_cocolux.svg') }}" alt=""
+                class="img-fluid w-100">
+        </div>
+        <div class="position-absolute w-100 d-flex justify-content-center view-2" >
+            <a href="{{ route('home') }}" class="d-flex gap-2 align-items-center"><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    xmlns="http://www.w3.org/2000/svg">
+                    <path d="M7.825 13L13.425 18.6L12 20L4 12L12 4L13.425 5.4L7.825 11H20V13H7.825Z" fill="black" />
+                </svg>
+                Quay lại trang chủ</a>
+        </div>
     </div>
 @endsection
 @section('link')
