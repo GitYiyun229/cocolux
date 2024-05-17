fc083ff9ababe94679713b8d534b3d447754b096
hoangthong2950 <hoangthong2950@gmail.com>
2024-05-17 15:58:31 +0700

    check 404

--- resources/views/web/404.blade.php
+++ resources/views/web/404.blade.php
@@ -7,9 +7,8 @@
                 class="img-fluid w-100">
         </div>
         <div class="position-absolute w-100 d-flex justify-content-center view-2" >
-            <a href="{{ route('home') }}" class="d-flex gap-2 align-items-center"><svg width="24" height="24" viewBox="0 0 24 24" fill="none"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path d="M7.825 13L13.425 18.6L12 20L4 12L12 4L13.425 5.4L7.825 11H20V13H7.825Z" fill="black" />
+            <a href="{{ route('home') }}" class="d-flex gap-2 align-items-center">
+                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">  <path d="M7.825 13L13.425 18.6L12 20L4 12L12 4L13.425 5.4L7.825 11H20V13H7.825Z" fill="black" />
                 </svg>
                 Quay lại trang chủ</a>
 
