40f815a126181f573b3f643e81e347adecb67634
hoangthong2950 <hoangthong2950@gmail.com>
2024-06-01 18:38:31 +0700

    sửa view order nhanh

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,12 +16,24 @@ $(".btn-value-order").click(function () {
     $(".modal-body #Phone_MemberPhone").text(value.customerMobile);
     $(".modal-body #Status_Name").text(value.statusName);
     let total_tamtinh = 0;
+    let voucherText = "";
+    if (value.couponCode !== null && value.moneyDiscount !== 0) {
+        voucherText =
+            value.couponCode +
+            "/" +
+            value.moneyDiscount.toLocaleString("vi-VN") +
+            "đ";
+    } else if (value.couponCode !== null) {
+        voucherText = value.couponCode;
+    }
 
-    $(".modal-body #id_voucher").text(
-        value.couponCode + "/" + value.moneyDiscount.toLocaleString("vi-VN")+'đ'
+    $(".modal-body #id_voucher").text(voucherText);
+    $(".modal-body #id_price_vanchuyen").text(
+        value.shipFee.toLocaleString("vi-VN") + "đ"
     );
-    $(".modal-body #id_price_vanchuyen").text(value.shipFee.toLocaleString("vi-VN")+'đ');
-    $(".modal-body #id_price_total").text(value.calcTotalMoney.toLocaleString("vi-VN")+'đ');
+    $(".modal-body #id_price_total").text(
+        value.calcTotalMoney.toLocaleString("vi-VN") + "đ"
+    );
 
     value.products.forEach(function (product) {
         // Ví dụ: thêm sản phẩm vào danh sách trong modal
@@ -29,12 +41,18 @@ $(".btn-value-order").click(function () {
         total_tamtinh += productTotal;
         $(".modal-body #products-list-order").append(
             `<tr>
-            <td class="overflow-hidden text-start py-2">${product.productName}</td>
-            <td class="py-2">${(product.price).toLocaleString('vi-VN')}đ</td>
+            <td class="overflow-hidden text-start py-2">${
+                product.productName
+            }</td>
+            <td class="py-2">${product.price.toLocaleString("vi-VN")}đ</td>
             <td class="py-2">${product.quantity}</td>
-            <td class="py-2">${(product.price * product.quantity).toLocaleString('vi-VN')}đ</td>
+            <td class="py-2">${(
+                product.price * product.quantity
+            ).toLocaleString("vi-VN")}đ</td>
             </tr>`
         );
     });
-  $(".modal-body #price_tamtinh").text(total_tamtinh.toLocaleString('vi-VN')+'đ');
+    $(".modal-body #price_tamtinh").text(
+        total_tamtinh.toLocaleString("vi-VN") + "đ"
+    );
 });
