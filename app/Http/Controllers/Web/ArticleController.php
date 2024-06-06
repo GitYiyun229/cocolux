4f48112b72e71fbe73ad53e9265022c377d58b9a
hoangthong2950 <hoangthong2950@gmail.com>
2024-06-06 11:56:14 +0700

    chỉnh sửa giá phần tin tức

--- app/Http/Controllers/Web/ArticleController.php
+++ app/Http/Controllers/Web/ArticleController.php
@@ -47,20 +47,20 @@ class ArticleController extends Controller
         SEOTools::twitter()->setSite('cocolux.com');
 
         $now = Carbon::now();
-        $cat_article = ArticlesCategories::where(['active'=> 1])->withDepth()->defaultOrder()->get()->toTree();
-        $article = $this->articleRepository->paginate(12,['id','slug','image','description','title','active','category_id','created_at'],['active'=>1]);
+        $cat_article = ArticlesCategories::where(['active' => 1])->withDepth()->defaultOrder()->get()->toTree();
+        $article = $this->articleRepository->paginate(12, ['id', 'slug', 'image', 'description', 'title', 'active', 'category_id', 'created_at'], ['active' => 1]);
         $article_hot = Article::where(['active' => 1, 'is_home' => 1])->limit(3)->get();
         $product_hots = ProductOptions::where(['active' => 1, 'is_default' => 1])
-            ->select('id','title','images','brand','hot_deal','sku','slug','parent_id','price','normal_price')
-            ->with(['product' => function($query){
-                $query->select('id','is_hot','slug');
-            },'promotionItem' => function($query) use ($now){
+            ->select('id', 'title', 'images', 'brand', 'hot_deal', 'sku', 'slug', 'parent_id', 'price', 'normal_price')
+            ->with(['product' => function ($query) {
+                $query->select('id', 'is_hot', 'slug');
+            }, 'promotionItem' => function ($query) use ($now) {
                 $query->where('applied_start_time', '<=', $now)->where('applied_stop_time', '>', $now)
                     ->orderBy('price', 'asc');
             }])->whereHas('product', function ($query) {
                 $query->where('is_hot', 1);
             })->limit(5)->get();
-        return view('web.article.home', compact('article','cat_article','product_hots','article_hot'));
+        return view('web.article.home', compact('article', 'cat_article', 'product_hots', 'article_hot'));
     }
 
     /**
@@ -68,40 +68,40 @@ class ArticleController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function cat($slug,$id)
+    public function cat($slug, $id)
     {
-        $category = ArticlesCategories::where(['id'=> $id,'active'=> 1])
-            ->select('id','title','seo_title','seo_keyword','seo_description')
+        $category = ArticlesCategories::where(['id' => $id, 'active' => 1])
+            ->select('id', 'title', 'seo_title', 'seo_keyword', 'seo_description')
             ->first();
         if (!$category) {
             abort(404);
         }
 
         $now = Carbon::now();
-        $cat_article = ArticlesCategories::where(['active'=> 1])->withDepth()->defaultOrder()->get()->toTree();
-        $article = $this->articleRepository->paginate(12,['id','slug','image','description','title','active','category_id','created_at'],['active'=>1,'category_id'=>$id]);
-        $article_hot = Article::where(['active' => 1, 'category_id' => $id])->limit(3)->orderBy('id','DESC')->get();
+        $cat_article = ArticlesCategories::where(['active' => 1])->withDepth()->defaultOrder()->get()->toTree();
+        $article = $this->articleRepository->paginate(12, ['id', 'slug', 'image', 'description', 'title', 'active', 'category_id', 'created_at'], ['active' => 1, 'category_id' => $id]);
+        $article_hot = Article::where(['active' => 1, 'category_id' => $id])->limit(3)->orderBy('id', 'DESC')->get();
         $product_hots = ProductOptions::where(['active' => 1, 'is_default' => 1])
-            ->select('id','title','images','brand','hot_deal','sku','slug','parent_id','price','normal_price')
-            ->with(['product' => function($query){
-                $query->select('id','is_hot','slug');
-            },'promotionItem' => function($query) use ($now){
+            ->select('id', 'title', 'images', 'brand', 'hot_deal', 'sku', 'slug', 'parent_id', 'price', 'normal_price')
+            ->with(['product' => function ($query) {
+                $query->select('id', 'is_hot', 'slug');
+            }, 'promotionItem' => function ($query) use ($now) {
                 $query->where('applied_start_time', '<=', $now)->where('applied_stop_time', '>', $now)
                     ->orderBy('price', 'asc');
             }])->whereHas('product', function ($query) {
                 $query->where('is_hot', 1);
             })->limit(5)->get();
 
-        SEOTools::setTitle($category->seo_title?$category->seo_title:$category->title);
-        SEOTools::setDescription($category->seo_description?$category->seo_description:$category->description);
-        SEOTools::addImages($category->image?asset($category->image):null);
+        SEOTools::setTitle($category->seo_title ? $category->seo_title : $category->title);
+        SEOTools::setDescription($category->seo_description ? $category->seo_description : $category->description);
+        SEOTools::addImages($category->image ? asset($category->image) : null);
         SEOTools::setCanonical(url()->current());
         SEOTools::opengraph()->setUrl(url()->current());
         SEOTools::opengraph()->addProperty('type', 'articles');
         SEOTools::twitter()->setSite('cocolux.com');
-        SEOMeta::setKeywords($category->seo_keyword?$category->seo_keyword:$category->title);
+        SEOMeta::setKeywords($category->seo_keyword ? $category->seo_keyword : $category->title);
 
-        return view('web.article.cat', compact('article','cat_article','product_hots','article_hot'));
+        return view('web.article.cat', compact('article', 'cat_article', 'product_hots', 'article_hot'));
     }
 
     /**
@@ -111,20 +111,36 @@ class ArticleController extends Controller
      */
     public function detail($slug, $id)
     {
-        $article = $this->articleRepository->getOneById($id,['category']);
+        $article = $this->articleRepository->getOneById($id, ['category']);
         $now = Carbon::now();
         preg_match_all('/product-option-(\d+)/', $article->content, $matches);
         $productOptionIds = $matches[1];
-        if (!empty($productOptionIds)){
-            $products = ProductOptions::whereIn('id', $productOptionIds)->select('id','slug','title','price','sku','images')->where('sku','!=',null)->where('slug','!=',null)->get();
+        if (!empty($productOptionIds)) {
+            $products = ProductOptions::whereIn('id', $productOptionIds)->select('id', 'slug', 'title', 'price', 'sku', 'images')->where('sku', '!=', null)->where('slug', '!=', null)->get();
             $productInfoMap = [];
             foreach ($products as $product) {
-                $productInfoMap[$product->id] = [
-                    'title' => $product->title,
-                    'image' => $product->image_first,
-                    'price' => format_money($product->price),
-                    'url' => route('detailProduct',['slug' => $product->slug, 'sku' => $product->sku]),
-                ];
+                $sku = $product->sku;
+                $product_sku = ProductOptions::where(['sku' => $sku])->with(['product' => function ($query) {
+                    $query->select('id', 'category_id', 'sku', 'slug', 'title', 'attributes', 'category_path', 'attribute_path', 'category_id', 'description', 'brand', 'seo_title', 'seo_keyword', 'seo_description', 'canonical_url');
+                }])->with(['promotionItem' => function ($query) use ($now) {
+                    $query->where('applied_start_time', '<=', $now)->where('applied_stop_time', '>', $now)
+                        ->orderBy('price', 'asc');
+                }])->where('sku', '!=', null)->first();
+                if($product_sku){
+                    $productInfoMap[$product->id] = [
+                        'title' => $product->title,
+                        'image' => $product->image_first,
+                        'price' => format_money($product_sku->promotionItem->price),
+                        'url' => route('detailProduct', ['slug' => $product->slug, 'sku' => $product->sku]),
+                    ];
+                }else{
+                    $productInfoMap[$product->id] = [
+                        'title' => $product->title,
+                        'image' => $product->image_first,
+                        'price' => format_money($product->price),
+                        'url' => route('detailProduct', ['slug' => $product->slug, 'sku' => $product->sku]),
+                    ];
+                }
             }
             $contentWithTitles = preg_replace_callback('/product-option-(\d+)/', function ($match) use ($productInfoMap) {
                 $productId = $match[1];
@@ -163,84 +179,84 @@ class ArticleController extends Controller
             $article->content = $contentWithTitles;
         }
 
-//        $promotions = $this->dealService->isFlashSaleAvailable();
-//        $promotions_flash_id = $promotions->pluck('id')->toArray();
-//        $applied_stop_time = $promotions->pluck('applied_stop_time','id')->toArray();
-//        $hot_deal = $this->dealService->isHotDealAvailable();
-//        $promotions_hot_id = $hot_deal->pluck('id')->toArray();
+        //        $promotions = $this->dealService->isFlashSaleAvailable();
+        //        $promotions_flash_id = $promotions->pluck('id')->toArray();
+        //        $applied_stop_time = $promotions->pluck('applied_stop_time','id')->toArray();
+        //        $hot_deal = $this->dealService->isHotDealAvailable();
+        //        $promotions_hot_id = $hot_deal->pluck('id')->toArray();
 
         $products_choose = null;
-        if ($article->products_up){
-            $id_products = explode(',',$article->products_up);
-            if ($article->updated_at < '2023-10-17'){
+        if ($article->products_up) {
+            $id_products = explode(',', $article->products_up);
+            if ($article->updated_at < '2023-10-17') {
                 $products_choose = ProductOptions::whereIn('parent_id', $id_products)
-                    ->select('id','title','images','brand','hot_deal','flash_deal','sku','slug','parent_id','price','normal_price')
-                    ->with(['product' => function($query){
-                        $query->select('id','is_hot','slug');
-                    },'promotionItem' => function($query) use ($now){
+                    ->select('id', 'title', 'images', 'brand', 'hot_deal', 'flash_deal', 'sku', 'slug', 'parent_id', 'price', 'normal_price')
+                    ->with(['product' => function ($query) {
+                        $query->select('id', 'is_hot', 'slug');
+                    }, 'promotionItem' => function ($query) use ($now) {
                         $query->where('applied_start_time', '<=', $now)->where('applied_stop_time', '>', $now)
                             ->orderBy('price', 'asc');
                     }])
-                    ->where('sku','!=',null)->where('slug','!=',null)->get();
-            }else{
+                    ->where('sku', '!=', null)->where('slug', '!=', null)->get();
+            } else {
                 $products_choose = ProductOptions::whereIn('id', $id_products)
-                    ->select('id','title','images','brand','hot_deal','flash_deal','sku','slug','parent_id','price','normal_price')
-                    ->with(['product' => function($query){
-                        $query->select('id','is_hot','slug');
-                    },'promotionItem' => function($query) use ($now){
+                    ->select('id', 'title', 'images', 'brand', 'hot_deal', 'flash_deal', 'sku', 'slug', 'parent_id', 'price', 'normal_price')
+                    ->with(['product' => function ($query) {
+                        $query->select('id', 'is_hot', 'slug');
+                    }, 'promotionItem' => function ($query) use ($now) {
                         $query->where('applied_start_time', '<=', $now)->where('applied_stop_time', '>', $now)
                             ->orderBy('price', 'asc');
                     }])
-                    ->where('sku','!=',null)->where('slug','!=',null)->get();
+                    ->where('sku', '!=', null)->where('slug', '!=', null)->get();
             }
         }
         $products_choose_down = null;
-        if ($article->products_down){
-            $id_products_down = explode(',',$article->products_down);
+        if ($article->products_down) {
+            $id_products_down = explode(',', $article->products_down);
             $products_choose_down = ProductOptions::whereIn('id', $id_products_down)
-                ->select('id','title','images','brand','hot_deal','flash_deal','sku','slug','parent_id','price','normal_price')
-                ->with(['product' => function($query){
-                    $query->select('id','is_hot','slug');
-                },'promotionItem' => function($query) use ($now){
+                ->select('id', 'title', 'images', 'brand', 'hot_deal', 'flash_deal', 'sku', 'slug', 'parent_id', 'price', 'normal_price')
+                ->with(['product' => function ($query) {
+                    $query->select('id', 'is_hot', 'slug');
+                }, 'promotionItem' => function ($query) use ($now) {
                     $query->where('applied_start_time', '<=', $now)->where('applied_stop_time', '>', $now)
                         ->orderBy('price', 'asc');
                 }])
-                ->where('sku','!=',null)->where('slug','!=',null)->get();
+                ->where('sku', '!=', null)->where('slug', '!=', null)->get();
         }
 
         $article_add = null;
-        if ($article->news_add){
-            $id_articles = explode(',',$article->news_add);
+        if ($article->news_add) {
+            $id_articles = explode(',', $article->news_add);
             $article_add = Article::whereIn('id', $id_articles)
-                ->select('id','title','image','slug','description')
-                ->where('active',1)->limit(3)->get();
+                ->select('id', 'title', 'image', 'slug', 'description')
+                ->where('active', 1)->limit(3)->get();
         }
 
-        $cat_article = ArticlesCategories::where(['active'=> 1])->withDepth()->defaultOrder()->get()->toTree();
-        $parent_cat = ArticlesCategories::select('id','title','slug')->where(['active'=> 1,'id' => $article->category_id])->first();
+        $cat_article = ArticlesCategories::where(['active' => 1])->withDepth()->defaultOrder()->get()->toTree();
+        $parent_cat = ArticlesCategories::select('id', 'title', 'slug')->where(['active' => 1, 'id' => $article->category_id])->first();
         $article_in_cat = Article::where(['active' => 1, 'category_id' => $article->category_id])
-            ->where('id','!=',$article->id)->limit(3)->orderBy('id','DESC')->get();
+            ->where('id', '!=', $article->id)->limit(3)->orderBy('id', 'DESC')->get();
         $product_hots = ProductOptions::where(['active' => 1, 'is_default' => 1])
-            ->select('id','title','images','brand','hot_deal','sku','slug','parent_id','price','normal_price')
-            ->with(['product' => function($query){
-                $query->select('id','is_hot','slug');
-            },'promotionItem' => function($query) use ($now){
+            ->select('id', 'title', 'images', 'brand', 'hot_deal', 'sku', 'slug', 'parent_id', 'price', 'normal_price')
+            ->with(['product' => function ($query) {
+                $query->select('id', 'is_hot', 'slug');
+            }, 'promotionItem' => function ($query) use ($now) {
                 $query->where('applied_start_time', '<=', $now)->where('applied_stop_time', '>', $now)
                     ->orderBy('price', 'asc');
             }])->whereHas('product', function ($query) {
                 $query->where('is_hot', 1);
             })->limit(5)->get();
 
-        SEOTools::setTitle($article->seo_title?$article->seo_title:$article->title);
-        SEOTools::setDescription($article->seo_description?$article->seo_description:$article->description);
-        SEOTools::addImages($article->image?asset($article->image):null);
+        SEOTools::setTitle($article->seo_title ? $article->seo_title : $article->title);
+        SEOTools::setDescription($article->seo_description ? $article->seo_description : $article->description);
+        SEOTools::addImages($article->image ? asset($article->image) : null);
         // SEOTools::setCanonical(url()->current());
         SEOTools::setCanonical($article->canonical_url ?? url()->current());
         SEOTools::opengraph()->setUrl(url()->current());
         SEOTools::opengraph()->addProperty('type', 'articles');
         SEOTools::twitter()->setSite('cocolux.com');
-        SEOMeta::setKeywords($article->seo_keyword?$article->seo_keyword:$article->title);
+        SEOMeta::setKeywords($article->seo_keyword ? $article->seo_keyword : $article->title);
 
-        return view('web.article.detail', compact('article','cat_article','article_in_cat','product_hots','parent_cat','products_choose','products_choose_down','article_add'));
+        return view('web.article.detail', compact('article', 'cat_article', 'article_in_cat', 'product_hots', 'parent_cat', 'products_choose', 'products_choose_down', 'article_add'));
     }
 }
