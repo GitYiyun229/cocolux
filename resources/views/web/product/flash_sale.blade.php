c28a8882d65e1f5aa5b234845431cd5608ea6d5d
hoangthong2950 <hoangthong2950@gmail.com>
2024-09-05 17:20:55 +0700

    sưa khung sale lần 2 tại trang chủ mục sp hot + trang chi tiết falsh deal

--- resources/views/web/product/flash_sale.blade.php
+++ resources/views/web/product/flash_sale.blade.php
@@ -27,13 +27,13 @@
                                         <img src="{{ asset($item->image_first) }}" alt="{{ $item->title }}"
                                             class="img-fluid">
                                         @if (!empty($item->image_deal))
-                                            <div class="position-absolute top-0 bottom-0">
+                                            <div class="position-absolute top-0 bottom-0 image-frame-top">
                                                 <img src="{{ asset(preg_replace('/\.(png|jpg|jpeg)$/i', '.webp', $item->image_deal)) }}"
                                                     alt="">
                                             </div>
                                         @else
                                             @if ($setting['frame_image_for_sale'])
-                                                <div class="position-absolute top-0 bottom-0"> <img
+                                                <div class="position-absolute top-0 bottom-0 image-frame-top"> <img
                                                         src="{{ asset($setting['frame_image_for_sale']) }}" alt="">
                                                 </div>
                                             @endif
