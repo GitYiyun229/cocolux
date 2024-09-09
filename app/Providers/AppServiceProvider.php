d3cb498b6cd8362e4b4749409340a15b7bfd2b13
hoangthong2950 <hoangthong2950@gmail.com>
2024-09-09 16:35:47 +0700

    sưa khung sale lần 2 tại trang chủ mục sp hot + trang chi tiết falsh deal29/9 view detail

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -37,8 +37,8 @@ class AppServiceProvider extends ServiceProvider
         $setting = null;
         $settings = null;
         $currentUrl = URL::current(); // Lấy URL hiện tại
-        $expectedUrl = "https://cocolux:8890"; // URL bạn đã cấu hình
-        // $expectedUrl = "https://cocolux.com"; // URL bạn đã cấu hình
+        // $expectedUrl = "https://cocolux:8890"; // URL bạn đã cấu hình
+        $expectedUrl = "https://cocolux.com"; // URL bạn đã cấu hình
         if (!Str::startsWith($currentUrl, $expectedUrl)) {
             dd(); // Hiển thị và dừng thực thi để kiểm tra $currentUrl và $expectedUrl
         }
