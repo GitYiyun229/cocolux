568f472c47fd93628af8b46d99d9f91f89c3c1ee
hoangthong2950 <hoangthong2950@gmail.com>
2024-12-24 17:46:20 +0700

    thong update api order thay thế idnhanh check = api  thay = idnhanh db web điều kiện nếu có haowcj không api

--- app/Http/Controllers/ApiNhanhController.php
+++ app/Http/Controllers/ApiNhanhController.php
@@ -137,17 +137,17 @@ class ApiNhanhController extends Controller
         $response = $client->post($this->linkApi . $api, [
             'form_params' => $this->request_params
         ]);
-        \Log::info([
-            'request_params' => $this->request_params,
-            'line' => __LINE__,
-            'method' => __METHOD__
-        ]);
+        // \Log::info([
+        //     'request_params' => $this->request_params,
+        //     'line' => __LINE__,
+        //     'method' => __METHOD__
+        // ]);
         $data = json_decode($response->getBody(), true);
-        \Log::info([
-            'data_receive' => $data,
-            'line' => __LINE__,
-            'method' => __METHOD__
-        ]);
+        // \Log::info([
+        //     'data_receive' => $data,
+        //     'line' => __LINE__,
+        //     'method' => __METHOD__
+        // ]);
         if ($data['code'] == 1) {
             return end($data['data']['products']);
         } else {
