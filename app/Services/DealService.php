0ea9f118d5394b8449d6922596960445d8ab0680
hoangthong2950 <hoangthong2950@gmail.com>
2025-02-22 13:56:28 +0700

    chinh link deal

--- app/Services/DealService.php
+++ app/Services/DealService.php
@@ -10,16 +10,16 @@ class DealService
     public function isFlashSaleAvailable($flash_id = null)
     {
         $now = Carbon::now();
-        if ($flash_id){
+        if ($flash_id) {
             $flash_sale = Promotions::where(['type' => 'flash_deal', 'id' => $flash_id])
                 ->where('applied_start_time', '<=', $now)
                 ->where('applied_stop_time', '>', $now)
-                ->select('id','name', 'code','thumbnail_url','applied_start_time','applied_stop_time','sort_product')->first();
-        }else{
+                ->select('id', 'name', 'code', 'thumbnail_url', 'applied_start_time', 'applied_stop_time', 'sort_product')->first();
+        } else {
             $flash_sale = Promotions::where(['type' => 'flash_deal'])
                 ->where('applied_start_time', '<=', $now)
                 ->where('applied_stop_time', '>', $now)
-                ->select('id','name', 'code','thumbnail_url','applied_start_time','applied_stop_time','sort_product')->get();
+                ->select('id', 'name', 'code', 'thumbnail_url', 'applied_start_time', 'applied_stop_time', 'sort_product')->get();
         }
 
         return $flash_sale;
@@ -28,16 +28,16 @@ class DealService
     public function isHotDealAvailable($hot_id = null)
     {
         $now = Carbon::now();
-        if ($hot_id){
+        if ($hot_id) {
             $hot_deal = Promotions::where(['type' => 'hot_deal', 'id' => $hot_id])
                 ->where('applied_start_time', '<=', $now)
                 ->where('applied_stop_time', '>', $now)
-                ->select('id','name', 'code','thumbnail_url','applied_start_time','applied_stop_time','sort_product')->orderBy('id','DESC')->first();
-        }else{
+                ->select('id', 'name', 'code', 'thumbnail_url', 'applied_start_time', 'applied_stop_time', 'link', 'sort_product')->orderBy('id', 'DESC')->first();
+        } else {
             $hot_deal = Promotions::where(['type' => 'hot_deal'])
                 ->where('applied_start_time', '<=', $now)
                 ->where('applied_stop_time', '>', $now)
-                ->select('id','name', 'code','thumbnail_url','applied_start_time','applied_stop_time','sort_product')->orderBy('id','DESC')->get();
+                ->select('id', 'name', 'code', 'thumbnail_url', 'applied_start_time', 'applied_stop_time', 'link', 'sort_product')->orderBy('id', 'DESC')->get();
         }
 
         return $hot_deal;
